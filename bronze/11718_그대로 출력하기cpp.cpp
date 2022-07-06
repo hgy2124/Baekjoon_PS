2bb46ffc304917d55257c13751e21bf647271305
unknown <hgy2124@naver.com>
2022-07-06 14:07:29 +0900

    11718 추가

---
+++ bronze/11718_그대로 출력하기cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		string s;
+		getline(cin, s);
+		if (s == "") {
+			break;
+		}
+		cout << s << endl;
+	}
+}
