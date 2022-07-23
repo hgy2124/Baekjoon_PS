97b57d3bbe399a05b3768e2e4e9694c3c944d772
unknown <hgy2124@naver.com>
2022-07-23 10:24:10 +0900

    2581 추가

---
+++ silver/2581_소수.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int m, n, sum = 0, cnt=1,first;
+	cin >> m;
+	cin >> n;
+
+	for (int i = m; i < n + 1; i++) {
+		for (int i2 = 2; i2 < i + 1; i2++) {
+			if (i % i2 == 0) {
+				if (i2 == i) {
+					sum += i;
+					if (cnt == 1)
+						first = i;
+					cnt=0;
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
+	if (cnt == 1)
+		cout << "-1";
+	else
+		cout << sum << "\n" << first;
+}
