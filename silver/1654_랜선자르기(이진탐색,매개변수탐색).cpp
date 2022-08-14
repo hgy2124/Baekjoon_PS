3d4c554d8874819501aca9752c8548ffa347081a
unknown <hgy2124@naver.com>
2022-08-14 23:53:06 +0900

    1654 추가

---
+++ silver/1654_랜선자르기(이진탐색,매개변수탐색).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	long long k, n;
+	cin >> k >> n;
+	
+	//랜선 길이 저장
+	vector<int> vt;
+	long long num;
+	for (int i = 0; i < k; i++) {
+		cin >> num;
+		vt.push_back(num);
+	}
+	
+	sort(vt.begin(), vt.end());
+	
+	long long max_num = vt[k - 1];
+	long long left = 1;
+	long long mid = (left + max_num) / 2;
+	long long sum = 0;
+
+	while (left <= max_num) {
+		sum = 0;
+		for (int i = 0; i < k; i++) {
+			sum += vt[i] / mid;
+		}
+		if (sum >= n) {
+			left = mid + 1;
+			mid = (left + max_num) / 2;
+		}
+		else if (sum < n) {
+			max_num = mid - 1;
+			mid = (left + max_num) / 2;
+		}
+			
+	}
+	cout << mid;
+}
