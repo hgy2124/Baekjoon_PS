82065f7a843fbd77ce5eacb54cf9911aae323bb2
unknown <hgy2124@naver.com>
2022-08-15 19:12:05 +0900

    2775 수정

--- bronze/2775_배열 만들어서 누적된 수 출력.cpp
+++ bronze/2775_배열 만들어서 누적된 수 출력.cpp
@@ -9,9 +9,22 @@ int main() {
 	int t;
 	cin >> t;
 	int k, n;
+	int arr[16][16]={0,};
+	for (int i = 0; i < 15; i++) {
+		for (int j = 1; j < 15; j++) {
+			if (i == 0) {
+				arr[i][j] = j;
+			}
+				
+			else {
+				for (int j2 = 1; j2 <= j; j2++)
+					arr[i][j] += arr[i - 1][j2];
+			}
+		}
+	}
 	while (t--) {
 		cin >> k>>n;
-		
-	}
 
+		cout << arr[k][n]<<"\n";
+	}
 }
