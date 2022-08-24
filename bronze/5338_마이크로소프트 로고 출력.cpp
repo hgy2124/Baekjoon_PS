b4f725f836ccd8f9369d354c78091fe721c45675
unknown <hgy2124@naver.com>
2022-08-24 11:29:23 +0900

    5338 추가

---
+++ bronze/5338_마이크로소프트 로고 출력.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "       _.-;;-._"<<'\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-;;-._|" << '\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-''-._|" << '\n';
+}
