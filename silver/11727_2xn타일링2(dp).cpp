7ea4ca09720fa905c7f8c0a261ec0feba6098b60
unknown <hgy2124@naver.com>
2022-09-01 11:05:18 +0900

    11727 추가

---
+++ silver/11727_2xn타일링2(dp).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+	int arr[1001]={0,1,3};
+	for (int i = 3; i <= n; i++) {
+		arr[i] = (arr[i-2]*2+arr[i-1])%10007;
+	}
+	cout << arr[n];
+}
