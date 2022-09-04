822855bc185d488408c834549f614fd69fd48a0a
unknown <hgy2124@naver.com>
2022-09-04 09:44:44 +0900

    2606 수정

--- silver/2606_바이러스(dfs, 그래프).cpp
+++ silver/2606_바이러스(dfs, 그래프).cpp
@@ -11,6 +11,17 @@ stack<int> s;
 bool visited[101];
 int cnt = 0;
 
+void dfs(int k) {
+	visited[k] = true;
+	s.push(k);
+	for (int i = 0; i < vt[k].size(); i++) {
+		if (visited[vt[k][i]] == 0) {
+			dfs(vt[k][i]);
+			cnt += 1;
+		}
+
+	}
+}
 
 int main() {
 	cin.tie(NULL);
@@ -18,14 +29,14 @@ int main() {
 
 	int node; cin >> node;
 	int edge; cin >> edge;
-	
+
 	int a, b;
 	for (int i = 0; i < edge; i++) {
 		cin >> a >> b;
 		vt[a].push_back(b);
 		vt[b].push_back(a);
 	}
-
-	cout << vt[2];
+	dfs(1);
+	cout << cnt;
 
 }
