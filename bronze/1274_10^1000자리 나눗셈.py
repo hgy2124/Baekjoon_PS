22529a5b97610ed0c740dcd200551014a99f5a82
unknown <hgy2124@naver.com>
2022-09-14 20:38:08 +0900

    5337 추가

--- bronze/1274_10^1000자리 나눗셈.py
+++
@@ -1,3 +0,0 @@
-n,m=map(int,input().split())
-print(n//m)
-print(int(n)%int(m))
---
+++ bronze/5337_웰컴.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << ".  .   ."<<'\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout<<"|/\\|(/.|(_.(_)[ | )(/.";
+
+}
