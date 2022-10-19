3da02190717e6584aaa9a162a99626ce72f78fb7
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2022-10-20 00:04:26 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << "\n" << "N2 Bomber      Heavy Fighter  Limited    21        "<<"\n";
+	cout << "J-Type 327     Light Combat   Unlimited  1         " << "\n" << "NX Cruiser     Medium Fighter Limited    18        "<<"\n";
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        " << "\n" << "Royal Cruiser  Light Combat   Limited    4         ";
+
+}
