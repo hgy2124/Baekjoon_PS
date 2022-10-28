30dd67563cd0a2999ce5e3f06f0c8532e69067eb
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2022-10-28 13:01:38 +0900

    [Silver III] Title: N과 M (7), Time: 476 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int n, m;
+int arr[9];
+bool visited[9] = { false };
+vector <int> vt;
+
+void dfs(int x) {
+	if (x == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 0; i < vt.size(); i++) {
+			arr[x] = vt[i];
+			dfs(x + 1);
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> m;
+	int num;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		vt.push_back(num);
+	}
+	sort(vt.begin(), vt.end());
+	dfs(0);
+}
