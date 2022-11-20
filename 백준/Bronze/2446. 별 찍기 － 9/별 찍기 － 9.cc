5949e7435bcb335a000ce6cd54a1eef6a36f4f48
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2022-11-20 21:31:23 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int i2 = 1; i2 < i; i2++)
+			cout << " ";
+		for (int j = 1; j <= 2 * (n - i) + 1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	for (int i = 1; i < n; i++) {
+		for (int i2 = (n-i); i2 > 1; i2--)
+			cout << " ";
+		for (int j = 1; j<=2*i+1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+}
