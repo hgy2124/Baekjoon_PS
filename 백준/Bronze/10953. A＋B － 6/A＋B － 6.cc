251216babca9020290335cc8a512c9a10fd2dd55
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2022-12-28 02:24:00 +0900

    [Bronze II] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <deque>
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int t; cin >> t;
+	int a, b;
+	char c;
+	while (t--) {
+		cin >> a >> c >> b;
+		cout << a + b<<"\n";
+	}
+
+}
