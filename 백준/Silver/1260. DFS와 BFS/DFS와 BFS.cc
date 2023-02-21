344d315483da2af4d821c296b8b25499c238e839
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2023-02-22 00:07:06 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <queue>
+#define ll long long
+
+using namespace std;
+
+int n, m,v,x,y,k;
+
+vector<int> vt[1001];
+bool visited[1001];
+
+queue <int> q;
+stack <int> s;
+
+void dfs(int a) {
+	visited[a] = 1;
+	cout << a << " ";
+	for (int i = 0; i < vt[a].size(); i++) {
+		if (!visited[vt[a][i]]) {
+			dfs(vt[a][i]);
+		}
+	}
+}
+
+void bfs(int b) {
+
+	visited[b] = 1;
+	q.push(b);
+
+	while (!q.empty()) {
+	    k = q.front();
+		cout << k << " ";
+		q.pop();
+		
+		for (int i = 0; i < vt[k].size(); i++) {
+			if (!visited[vt[k][i]]) {
+				q.push(vt[k][i]);
+				visited[vt[k][i]]=1;
+			}
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> m >> v;
+
+	
+	for (int i = 0; i < m; i++) {
+		cin >> x >> y;
+		vt[x].push_back(y); //dfs용
+		vt[y].push_back(x);
+	}
+	for (int i = 0; i < 1001; i++)
+		sort(vt[i].begin(), vt[i].end());
+	//dfs
+	dfs(v);
+	
+
+	//bfs
+	memset(visited, 0, sizeof(visited));
+	cout <<"\n";
+	bfs(v);
+	
+}
