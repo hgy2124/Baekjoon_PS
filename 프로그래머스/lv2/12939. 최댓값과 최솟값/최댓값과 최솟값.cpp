a05c9b7095c1fa5559a72860e9e4d9753a2422bb
hgy2124 <83022306+hgy2124@users.noreply.github.com>
2023-02-28 13:38:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -6,24 +6,18 @@
 
 using namespace std;
 
-#define INT_MAX 210000000;
-#define INT_MIN -210000000;
-
 string solution(string s) {
     string answer = "";
-    char sep = ' '; //공백을 조건으로 주려고
-    istringstream iss(s);//입력스트림
-    string buf;//저장해놓을 버퍼
+    istringstream iss(s);
+    char space = ' ';
+    string buf;
     vector <int> vt;
     
-    int MIN = INT_MAX;
-    int MAX = INT_MIN;
-    while(getline(iss, buf, sep)) {
+    while(getline(iss,buf,space)){
         vt.push_back(stoi(buf));
     }
     sort(vt.begin(),vt.end());
-    string min_str = to_string(vt.front());
-    string max_str = to_string(vt.back());
-    answer = min_str + " " + max_str;
+    answer=to_string(vt.front()) + " " + to_string(vt.back());
+    
     return answer;
 }
